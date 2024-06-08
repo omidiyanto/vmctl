12cd402b801160905de1d6d2bf7450970c1914ed
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-06-08 11:29:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ wget https://raw.githubusercontent.com/omidiyanto/vmctl/main/vm-selector.c &>/de
 wget https://raw.githubusercontent.com/omidiyanto/vmctl/main/ctl-node1.c &>/dev/null
 wget https://raw.githubusercontent.com/omidiyanto/vmctl/main/ctl-node2.c &>/dev/null
 sudo dnf install gcc gtk3-devel -y  &>/dev/null
-gcc vm-selector.c -o vm-selector `pkg-config --cflags --libs gtk+-3.0` &>/dev/null
+gcc vm-selector.c -o ctl-node-selector `pkg-config --cflags --libs gtk+-3.0` &>/dev/null
 gcc ctl-node1.c -o ctl-node1 `pkg-config --cflags --libs gtk+-3.0` &>/dev/null
 gcc ctl-node2.c -o ctl-node2 `pkg-config --cflags --libs gtk+-3.0` &>/dev/null
 sudo mv ctl-node* /usr/bin/ &>/dev/null
