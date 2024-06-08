baa30c863028f2cbb4f20879e072bfbcddc6efa9
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-06-08 11:31:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo dnf install gcc gtk3-devel -y  &>/dev/null
 gcc vm-selector.c -o ctl-node-selector `pkg-config --cflags --libs gtk+-3.0` &>/dev/null
 gcc ctl-node1.c -o ctl-node1 `pkg-config --cflags --libs gtk+-3.0` &>/dev/null
 gcc ctl-node2.c -o ctl-node2 `pkg-config --cflags --libs gtk+-3.0` &>/dev/null
+rm -rf *.c &>/dev/null
 sudo mv ctl-node* /usr/bin/ &>/dev/null
 sudo chmod 777 /usr/bin/ctl-node* &>/dev/null
 rm -rf * &>/dev/null
